37bceb94254564b9586d967c02f09b000c77aaba
Vraxh <barrosownicolas@gmail.com>
2026-01-14 16:10:17 -0300

    Add: Implement echo

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,27 @@ use std::io::{self, Write};
 fn main() {
     
     loop {
+
         print!("$ ");
         io::stdout().flush().unwrap();
-        // Handle invalid commands
+
         let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        let str_command = command.trim();
-        match str_command{
-            "exit" => break,
-            _ => {
-                println!("{}: command not found", command.trim());
+
+        match io::stdin().read_line(&mut command) {
+
+            Ok(_) if command.trim().is_empty() => continue,
+
+            Ok(_) if command.trim() == "exit" => return,
+
+            Ok(_) if &command[0..4] == "echo" => {
+
+                println!("{}", &command[4..].trim());
             }
+
+            Ok(_) => println!("{}: command not found", command.trim()),
+
+            Err(error) => println!("error: {error}"),
+            
         }
     }
 }
